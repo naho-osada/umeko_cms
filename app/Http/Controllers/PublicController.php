34813b95ed6c2500a1c2bdd1d4e63dc7fa0b137e
Naho Osada <clover.kuroi@gmail.com>
2022-03-02 12:45:41 +0900

    目次機能を追加 #1

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -71,6 +71,50 @@ class PublicController extends Controller
     {
         // middlewareで取得済み
         $article = $request->article;
+
+        // 記事内容に目次を付ける h2～h4まで自動生成
+        preg_match_all("/<h[2-4](.*?)>(.*?)<\/h[2-4]>/i",$article->contents, $tags, PREG_PATTERN_ORDER);
+        $contents = $article->contents;
+        // 目次用変数
+        $tableOfContents = [];
+        $loopCnt = count($tags[0]);
+        $headKeyCnt[] = '';
+        $headKeyCnt[2] = 0;
+        $headKeyCnt[3] = 0;
+        $headKeyCnt[4] = 0;
+        for($i=0; $i<$loopCnt; $i++) {
+            $headTag = $tags[0][$i];
+            $attr = $tags[1][$i];
+            $str = $tags[2][$i];
+
+            // attrにID指定が含まれる場合は目次から除外する（IDの複数指定はできないので、編集を優先する）
+            if(strpos($attr, 'id="') !== false) continue;
+            // h2のとき
+            if(strpos($headTag, 'h2') !== false) {
+                $headKey = 2;
+            } else if(strpos($headTag, 'h3') !== false) {
+                $headKey = 3;
+            } else if(strpos($headTag, 'h4') !== false) {
+                $headKey = 4;
+            }
+            $headKeyCnt[$headKey]++;
+            if(!empty($attr)) {
+                $attrs = explode('class="', $attr);
+                $attrClass = str_replace('"', '', $attrs[1]) . ' lv-' . $headKey;
+            } else {
+                $attrClass = 'lv-' . $headKey;
+            }
+
+            $contents = str_replace($headTag, '<h' . $headKey . $attr . ' id="ttl-' . $headKey . '-' . $headKeyCnt[$headKey] . '">' . $str . '</h' . $headKey . '>', $contents);
+            $tableOfContents[] = '<li class="' . $attrClass .'"><a href="#ttl-' . $headKey . '-' . $headKeyCnt[$headKey] . '">' . $str . '</a></li>';
+        }
+        if(!empty($tableOfContents)) {
+            // 目次が存在する場合、「初めのh2タグの上」に目次を追加する
+            $firstH2Tag = explode('<h2',$contents);
+            $contents = $firstH2Tag[0] . '<div class="table-of-contents"><h2>目次</h2><ul>' . implode('', $tableOfContents) . '</ul></div><h2' . $firstH2Tag[1];
+        }
+        $article->contents = $contents;
+
         $db = new Article();
 
         // カテゴリ情報の取得
